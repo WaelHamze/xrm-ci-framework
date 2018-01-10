27585d62eadcb63a0cbb95b1c5e2f726767be2f1
Wael Hamze <contact@waelhamze.com>
2018-01-10 18:24:55 +0000

    Deleted unwanted files

--- BuildTools/Xrm.CI.Framework.BuildTools/Lib/xRMCIFramework/9.0.0/DeployPackage.ps1
+++ BuildTools/Xrm.CI.Framework.BuildTools/Lib/xRMCIFramework/9.0.0/DeployPackage.ps1
@@ -25,6 +25,20 @@ Write-Verbose "PackageDeploymentPath = $PackageDeploymentPath"
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 Write-Verbose "Script Path: $scriptPath"
 
+#Set Security Protocol
+$currentProtocol = [System.Net.ServicePointManager]::SecurityProtocol
+Write-Verbose "Current Security Protocol: $currentProtocol"
+if (-not $currentProtocol.HasFlag([System.Net.SecurityProtocolType]::Tls11))
+{
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol  -bor [System.Net.SecurityProtocolType]::Tls11;
+}
+if (-not $currentProtocol.HasFlag([System.Net.SecurityProtocolType]::Tls12))
+{
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol  -bor [System.Net.SecurityProtocolType]::Tls12;
+}
+$currentProtocol = [System.Net.ServicePointManager]::SecurityProtocol
+Write-Verbose "Modified Security Protocol: $currentProtocol"
+
 #Load XRM Tooling
 
 $crmToolingConnector = $scriptPath + "\Microsoft.Xrm.Tooling.CrmConnector.Powershell.dll"
