1958afe48821507f1fef80c205fe83976ad6982f
Sagar Gaikwad <sagarhgaikwad@gmail.com>
2018-04-16 20:32:46 +0530

    Cmdlet to Add all components from one solution to another solution

---
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/MoveXrmSolutionComponentsCommand.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Xrm.Framework.CI.PowerShell.Cmdlets.Common;
+
+namespace Xrm.Framework.CI.PowerShell.Cmdlets
+{
+    /// <summary>
+    /// <para type="synopsis">Moves a CRM Solution Components.</para>
+    /// <para type="description">The Move-XrmSolutionComponents of a CRM solution to another by unique name.
+    /// </para>
+    /// </summary>
+    /// <example>
+    ///   <code>C:\PS>Move-XrmSolutionComponents -ConnectionString "" -FromSolutionName "UniqueSolutionName -ToSolutionName "UniqueSolutionName"</code>
+    ///   <para>Exports the "" managed solution to "" location</para>
+    /// </example>
+    [Cmdlet(VerbsCommon.Move, "XrmSolutionComponents")]
+    public class MoveXrmSolutionComponentsCommand : XrmCommandBase
+    {
+        #region Parameters
+
+        /// <summary>
+        /// <para type="description">The unique name of the solution components to be moved from.</para>
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        public string FromSolutionName { get; set; }
+
+        /// <summary>
+        /// <para type="description">The unique name of the solution components to be moved to.</para>
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        public string ToSolutionName { get; set; }
+
+        #endregion
+
+        #region Process Record
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            base.WriteVerbose(string.Format("Moving Solution Components from {0} to {1}", FromSolutionName, ToSolutionName));
+
+            using (var context = new CIContext(OrganizationService))
+            {
+                var fromSolutionId = GetSolutionId(context, FromSolutionName);
+                var toSolutionId = GetSolutionId(context, ToSolutionName);
+
+                var fromSolutionComponents = (from s in context.SolutionComponentSet
+                            where s.SolutionId == new EntityReference(Solution.EntityLogicalName, fromSolutionId)
+                            orderby s.RootSolutionComponentId descending
+                            select new { s.Id, s.ComponentType, s.ObjectId, s.RootSolutionComponentId, s.IsMetadata }).ToList();
+                var toSolutionComponents = (from s in context.SolutionComponentSet
+                            where s.SolutionId == new EntityReference(Solution.EntityLogicalName, toSolutionId)
+                            select new { s.Id, s.ComponentType, s.ObjectId, s.RootSolutionComponentId }).ToList();
+
+                foreach (var solutionComponent in fromSolutionComponents)
+                {                   
+                    if (toSolutionComponents.Exists(depen => depen.ObjectId == solutionComponent.ObjectId && depen.ComponentType.Value == solutionComponent.ComponentType.Value))
+                    {
+                        continue;
+                    }
+
+                    var addReq = new AddSolutionComponentRequest()
+                    {
+                        ComponentId = (Guid)solutionComponent.ObjectId,
+                        ComponentType = (int)solutionComponent.ComponentType.Value,
+                        AddRequiredComponents = false,                        
+                        SolutionUniqueName = ToSolutionName
+                    };
+
+                    if ((solutionComponent.IsMetadata ?? false) && fromSolutionComponents.Exists(depen => depen.RootSolutionComponentId == solutionComponent.Id))
+                    {
+                        addReq.DoNotIncludeSubcomponents = true;
+                        base.WriteVerbose("DoNotIncludeSubcomponents set to true");
+                    }
+
+                    OrganizationService.Execute(addReq);
+                    base.WriteVerbose(string.Format("Moved component from solution with Id : {0} and Type : {1}", solutionComponent.ObjectId, solutionComponent.ComponentType.Value));
+                }
+            }
+        }
+
+        private Guid GetSolutionId(CIContext context, string solutionName)
+        {
+            var query1 = from solution in context.SolutionSet
+                         where solution.UniqueName == solutionName
+                         select solution.Id;
+
+            if (query1 == null)
+            {
+                throw new Exception(string.Format("Solution {0} could not be found", solutionName));
+            }
+
+            var solutionId = query1.FirstOrDefault();
+
+            return solutionId;
+        }
+
+        #endregion
+    } 
+}
---
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/MoveSolutionComponents.ps1
@@ -0,0 +1,33 @@
+﻿#
+# Filename: ExtractSolution.ps1
+#
+param(
+[string]$FromSolutionName="TestPluginRegistration", #The unique CRM solution name
+[string]$ToSolutionName="Test1", #The unique CRM solution name
+[string]$CrmConnectionString="AuthType=Office365;Username=sagarh@dracola.onmicrosoft.com; Password=Sagar@123;Url=https://draco2.crm8.dynamics.com", #The connection string as per CRM Sdk
+[int]$Timeout=360
+) 
+
+$ErrorActionPreference = "Stop"
+
+Write-Verbose 'Entering MoveSolutionComponent.ps1'
+
+Write-Verbose "FromSolutionName = $FromSolutionName"
+Write-Verbose "ToSolutionName = $ToSolutionName"
+Write-Verbose "ConnectionString = $CrmConnectionString"
+Write-Verbose "Timeout = $Timeout"
+
+#Script Location
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+Write-Verbose "Script Path: $scriptPath"
+
+#Load XrmCIFramework
+$xrmCIToolkit = $scriptPath + "\Xrm.Framework.CI.PowerShell.Cmdlets.dll"
+$xrmCIToolkit ="C:\Users\Sagar Gaikwad\source\repos\xrm-ci-framework\MSDYNV9\Xrm.Framework.CI\Xrm.Framework.CI.PowerShell.Cmdlets\bin\Debug\Xrm.Framework.CI.PowerShell.Cmdlets.dll"
+Write-Verbose "Importing CIToolkit: $xrmCIToolkit" 
+Import-Module $xrmCIToolkit
+Write-Verbose "Imported CIToolkit"
+
+Move-XrmSolutionComponents -FromSolutionName $FromSolutionName -ToSolutionName $ToSolutionName -ConnectionString $CrmConnectionString -Timeout $Timeout -Verbose
+
+Write-Verbose 'Leaving MoveSolutionComponent.ps1'
