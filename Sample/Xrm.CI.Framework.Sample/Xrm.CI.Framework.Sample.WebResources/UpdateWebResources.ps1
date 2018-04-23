63a1dadce7effd51042b3dc2d376d360271a161f
Wael Hamze <contact@waelhamze.com>
2018-04-23 22:44:14 +0100

    Added Script to Update Web Resources

---
+++ Sample/Xrm.CI.Framework.Sample/Xrm.CI.Framework.Sample.WebResources/UpdateWebResources.ps1
@@ -0,0 +1,28 @@
+﻿[CmdletBinding()]
+
+param
+(
+    [string]$CrmConnectionString #The connection string as per CRM Sdk
+)
+
+$ErrorActionPreference = "Stop"
+
+#Script Location
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+Write-Verbose "Script Path: $scriptPath"
+
+Write-Verbose "ConnectionString = $connectionString"
+
+if ($CrmConnectionString -eq '')
+{
+	$CrmConnectionString = $Env:CrmCon
+}
+$AssemblyName = 'Xrm.CI.Framework.Sample.Plugins'
+$WebResourceFolderPath = "$scriptPath\WebResources"
+$CommaSeparatedWebResourceExtensions = '*.html,*.js'
+$RegExToMatchUniqueName = '$fileName'
+$IncludeFileExtensionForUniqueName = $true
+$Publish = $true
+#$Timeout = 120
+
+& "$scriptPath\..\packages\XrmCIFramework.9.0.0.16\tools\UpdateFoldersWebResources.ps1" -Verbose -CrmConnectionString "$CrmConnectionString" -WebResourceFolderPath $WebResourceFolderPath -CommaSeparatedWebResourceExtensions $CommaSeparatedWebResourceExtensions -RegExToMatchUniqueName $RegExToMatchUniqueName -IncludeFileExtensionForUniqueName $IncludeFileExtensionForUniqueName -Publish $true
