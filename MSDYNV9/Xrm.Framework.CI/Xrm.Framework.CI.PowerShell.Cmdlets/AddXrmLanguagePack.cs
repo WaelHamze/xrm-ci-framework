169b9e4f621af13c2ed0c9df68a47c82ba439b00
Mariusz Jakubowski <mariusz.jakubowski@pwc.com>
2018-08-19 13:26:47 +0200

    add/get xrm language packs cmdlets

---
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/AddXrmLanguagePack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Xrm.Framework.CI.PowerShell.Cmdlets
+{
+    /// <summary>
+    /// <para type="synopsis">Provision a new language on CRM server</para>
+    /// <para type="description">The Add-XrmLanguagePack cmdlet provision a new language on CRM server. Language pack must be installed before on the server.</para>
+    /// <para type="description">It's recommended to set a high timeout (Timeout parameter).</para>
+    /// <para type="description">Executes a "ProvisionLanguageRequest".</para>
+    /// </summary>
+    /// <example>
+    ///   <code>C:\PS>Add-XrmLanguagePack -Language 1045 -ConnectionString $connectionString -Timeout 900</code>
+    ///   <para>Provision a new language on CRM server.</para>
+    /// </example>
+    [Cmdlet(VerbsCommon.Add, "XrmLanguagePack")]
+    [OutputType(typeof(bool))]
+    public class AddXrmLanguagePack : XrmCommandBase
+    {
+        /// <summary>
+        /// <para type="description">Sets the language to provision. Required.</para>
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        public int Language { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            var availableLanguagesPacks = ((RetrieveProvisionedLanguagesResponse)OrganizationService.Execute(new RetrieveProvisionedLanguagesRequest())).RetrieveProvisionedLanguages;
+            var installedLanguagePacks = ((RetrieveInstalledLanguagePacksResponse)OrganizationService.Execute(new RetrieveInstalledLanguagePacksRequest())).RetrieveInstalledLanguagePacks;
+
+            if (availableLanguagesPacks.Contains(Language))
+            {
+                WriteVerbose($"LanguagePack {Language} already provisioned");
+                WriteObject(true);
+                return;
+            }
+
+            if (!installedLanguagePacks.Contains(Language))
+            {
+                throw new ArgumentException($"LanguagePack {Language} is not installed on server", nameof(Language));
+            }
+
+            if (Timeout == 0)
+            {
+                WriteWarning($"It's recommended to use Timeout parameter for this cmdlet. Language provisioning will continue on server if this cmdlet timeout.");
+            }
+
+            OrganizationService.Execute(new ProvisionLanguageRequest
+            {
+                Language = Language
+            });
+            WriteVerbose($"LanguagePack {Language} sucessfully provisioned.");
+        }
+    }
+}
---
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/GetXrmLanguagePack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Management.Automation;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Xrm.Framework.CI.PowerShell.Cmdlets
+{
+    /// <summary>
+    /// <para type="synopsis">Return available (provisioned) or installed language packs on CRM server</para>
+    /// <para type="description">The Get-XrmLanguagePack cmdlet returns list of available (provisioned) or installed language packs on server.</para>
+    /// <para type="description">Executes a "RetrieveProvisionedLanguagesRequest" or "RetrieveInstalledLanguagePacksRequest".</para>
+    /// </summary>
+    /// <example>
+    ///   <code>C:\PS>Get-XrmLanguagePack -ConnectionString $connectionString -Type Installed</code>
+    ///   <para>Returns list of language packs.</para>
+    /// </example>
+    [Cmdlet(VerbsCommon.Get, "XrmLanguagePack")]
+    [OutputType(typeof(int[]))]
+    public class GetXrmLanguagePack : XrmCommandBase
+    {
+        /// <summary>
+        /// <para type="description">Type of result - Installed or Available/Provisioned</para>
+        /// </summary>
+        [Parameter(Mandatory = false)]
+        public LanguagePackGetType Type { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            if (Type == LanguagePackGetType.Installed)
+            {
+                WriteVerbose("Installed language packs");
+                WriteObject(((RetrieveInstalledLanguagePacksResponse)OrganizationService.Execute(new RetrieveInstalledLanguagePacksRequest())).RetrieveInstalledLanguagePacks);
+            }
+            else
+            {
+                WriteVerbose("Available (provisioned) language packs");
+                WriteObject(((RetrieveProvisionedLanguagesResponse)OrganizationService.Execute(new RetrieveProvisionedLanguagesRequest())).RetrieveProvisionedLanguages);
+            }
+        }
+    }
+
+    public enum LanguagePackGetType
+    {
+        Installed,
+        Available,
+        Provisioned
+    }
+}
