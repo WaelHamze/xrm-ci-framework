0db87e81b073112d866a6f1b47f69bc2d421aaab
Wael Hamze <contact@waelhamze.com>
2018-09-13 20:02:48 +0100

    Fix for TLS 1.2 issue with Online Management API

--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/DeployPackage.ps1
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/DeployPackage.ps1
@@ -32,18 +32,7 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 Write-Verbose "Script Path: $scriptPath"
 
 #Set Security Protocol
-$currentProtocol = [System.Net.ServicePointManager]::SecurityProtocol
-Write-Verbose "Current Security Protocol: $currentProtocol"
-if (-not $currentProtocol.HasFlag([System.Net.SecurityProtocolType]::Tls11))
-{
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol  -bor [System.Net.SecurityProtocolType]::Tls11;
-}
-if (-not $currentProtocol.HasFlag([System.Net.SecurityProtocolType]::Tls12))
-{
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol  -bor [System.Net.SecurityProtocolType]::Tls12;
-}
-$currentProtocol = [System.Net.ServicePointManager]::SecurityProtocol
-Write-Verbose "Modified Security Protocol: $currentProtocol"
+& "$scriptPath\SetTlsVersion.ps1"
 
 #Load XRM Tooling
 
