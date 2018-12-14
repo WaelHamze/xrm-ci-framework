36e699395b4d9339625b2e9eef90176a34693c43
Vladimir Goncharov <vladimir.goncharov@gmail.com>
2018-12-14 11:40:11 +0100

    Extended the cmdlet to publish a duplicate detection rule by a name

--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/PublishXrmDuplicateRuleCommand.cs
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/PublishXrmDuplicateRuleCommand.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Management.Automation;
 using Microsoft.Crm.Sdk.Messages;
+using Xrm.Framework.CI.PowerShell.Cmdlets.Common;
 
 namespace Xrm.Framework.CI.PowerShell.Cmdlets
 {
@@ -13,6 +15,10 @@ namespace Xrm.Framework.CI.PowerShell.Cmdlets
     ///   <code>C:\PS>Publish-XrmDuplicateRule -ConnectionString "" -DuplicateRuleId $duplicateRuleId</code>
     ///   <para>Publishes a existing CRM duplicate rule with the Id $duplicateRuleId</para>
     /// </example>
+    /// <example>
+    ///   <code>C:\PS>Publish-XrmDuplicateRule -ConnectionString "" -DuplicateRuleName $duplicateRuleName</code>
+    ///   <para>Publishes a existing CRM duplicate rule with the name $duplicateRuleName</para>
+    /// </example>
     [Cmdlet(VerbsData.Publish, "XrmDuplicateRule")]
     public class PublishXrmDuplicateRuleCommand : XrmCommandBase
     {
@@ -21,8 +27,14 @@ namespace Xrm.Framework.CI.PowerShell.Cmdlets
         /// <summary>
         /// Id of a CRM duplicate rule to be published
         /// </summary>
-        [Parameter(Mandatory = true)]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "PublishByDuplicateRuleId")]
         public Guid DuplicateRuleId { get; set; }
+
+        /// <summary>
+        /// Name of a CRM duplicate rule to be published
+        /// </summary>
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "PublishByDuplicateRuleName")]
+        public string DuplicateRuleName { get; set; }
 
         #endregion
 
@@ -32,15 +44,45 @@ namespace Xrm.Framework.CI.PowerShell.Cmdlets
         {
             base.ProcessRecord();
 
-            base.WriteVerbose(string.Format("Publishing a CRM duplicate rule with Id: {0}", DuplicateRuleId));
+            Guid duplicateRuleId = DuplicateRuleId != Guid.Empty ? DuplicateRuleId : GetDuplicateRuleId(DuplicateRuleName);
+
+            base.WriteVerbose(string.Format("Publishing a CRM duplicate rule with Id: {0}", duplicateRuleId));
 
             var publishAllXmlRequest = new PublishDuplicateRuleRequest
             {
-                DuplicateRuleId = DuplicateRuleId
+                DuplicateRuleId = duplicateRuleId
             };
             OrganizationService.Execute(publishAllXmlRequest);
 
             base.WriteVerbose("Publish duplicate rule request sent out successfully");
+        }
+
+        private Guid GetDuplicateRuleId(string duplicateRuleName)
+        {
+            using (var context = new CIContext(OrganizationService))
+            {
+                base.WriteVerbose(string.Format("Querying a duplicate rule with the name: {0}", duplicateRuleName));
+
+                var query = from rule in context.DuplicateRuleSet
+                    where rule.Name == duplicateRuleName
+                    select new DuplicateRule
+                    {
+                        Id = rule.Id,
+                        Name = rule.Name
+                    };
+
+                DuplicateRule duplicateRule = query.FirstOrDefault();
+
+                if (duplicateRule == null)
+                {
+                    var error = string.Format("Duplicate rule with the name '{0}' could not be found", duplicateRuleName);
+                    throw new ArgumentOutOfRangeException(nameof(duplicateRuleName), error);
+                }
+
+                base.WriteVerbose(string.Format("Found a duplicate rule with the Id: {0}", duplicateRule.Id));
+
+                return duplicateRule.Id;
+            }
         }
 
         #endregion
