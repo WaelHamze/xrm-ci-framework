3998b3cabfd06aada8e1e355d039d86d54028d0f
Wael Hamze <contact@waelhamze.com>
2018-12-18 17:50:15 +0000

    Improvements to Patch Creation Cmdlet/Script

--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/CreatePatch.ps1
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/CreatePatch.ps1
@@ -2,21 +2,21 @@
 # Filename: CloneSolutionAsPatch.ps1
 #
 param(
-[string]$DisplayName, #Patched solution display name
-[string]$ParentSolutionUniqueName, #The unique name of parent CRM solution
-[string]$VersionNumber, #The version number of patched solution and has to be greater than parent solution
 [string]$CrmConnectionString, #The connection string as per CRM Sdk
+[string]$ParentSolutionUniqueName, #The unique name of parent CRM solution
+[string]$DisplayName, #Patched solution display name
+[string]$VersionNumber, #The version number of patched solution and has to be greater than parent solution
 [int]$Timeout=360
 ) 
 
 $ErrorActionPreference = "Stop"
 
-Write-Verbose 'Entering CloneSolutionAsPatch.ps1'
+Write-Verbose 'Entering CreatePatch.ps1'
 
-Write-Verbose "DisplayName = $DisplayName"
-Write-Verbose "ParentSolutionUniqueName = $ParentSolutionUniqueName"
-Write-Verbose "VersionNumber = $VersionNumber"
 Write-Verbose "ConnectionString = $CrmConnectionString"
+Write-Verbose "ParentSolutionUniqueName = $ParentSolutionUniqueName"
+Write-Verbose "DisplayName = $DisplayName"
+Write-Verbose "VersionNumber = $VersionNumber"
 Write-Verbose "Timeout = $Timeout"
 
 #Script Location
@@ -29,6 +29,8 @@ Write-Verbose "Importing CIToolkit: $xrmCIToolkit"
 Import-Module $xrmCIToolkit
 Write-Verbose "Imported CIToolkit"
 
-Add-XrmSolutionPatch -DisplayName $DisplayName -ParentSolutionUniqueName $ParentSolutionUniqueName -VersionNumber $VersionNumber -ConnectionString $CrmConnectionString -Timeout $Timeout -Verbose
+$patchName = Add-XrmSolutionPatch -DisplayName $DisplayName -ParentSolutionUniqueName $ParentSolutionUniqueName -VersionNumber $VersionNumber -ConnectionString $CrmConnectionString -Timeout $Timeout
 
-Write-Verbose 'Leaving CloneSolutionAsPatch.ps1'
+Write-Host ("Patch solution created with name: {0}" -f $patchName)
+
+Write-Verbose 'Leaving CreatePatch.ps1'
