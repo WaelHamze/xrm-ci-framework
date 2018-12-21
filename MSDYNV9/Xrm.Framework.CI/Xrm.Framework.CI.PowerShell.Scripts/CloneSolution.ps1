84a0db7bada7309b6bd8adc8c4562bc5089f18c5
Wael Hamze <contact@waelhamze.com>
2018-12-21 22:36:22 +0000

    Enhancements to Clone Solution

--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/CloneSolution.ps1
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/CloneSolution.ps1
@@ -1,22 +1,22 @@
 #
-# Filename: CloneSolutionAsSolution.ps1
+# Filename: CloneSolution.ps1
 #
 param(
-[string]$DisplayName, #Solution display name
-[string]$ParentSolutionUniqueName, #The unique name of parent CRM solution
-[string]$VersionNumber, #The version number of solution and has to be greater than parent solution
 [string]$CrmConnectionString, #The connection string as per CRM Sdk
+[string]$ParentSolutionUniqueName, #The unique name of parent CRM solution
+[string]$DisplayName, #Solution display name
+[string]$VersionNumber, #The version number of solution and has to be greater than parent solution
 [int]$Timeout=360
 ) 
 
 $ErrorActionPreference = "Stop"
 
-Write-Verbose 'Entering CloneSolutionAsSolution.ps1'
+Write-Verbose 'Entering CloneSolution.ps1'
 
-Write-Verbose "DisplayName = $DisplayName"
-Write-Verbose "ParentSolutionUniqueName = $ParentSolutionUniqueName"
-Write-Verbose "VersionNumber = $VersionNumber"
 Write-Verbose "ConnectionString = $CrmConnectionString"
+Write-Verbose "ParentSolutionUniqueName = $ParentSolutionUniqueName"
+Write-Verbose "DisplayName = $DisplayName"
+Write-Verbose "VersionNumber = $VersionNumber"
 Write-Verbose "Timeout = $Timeout"
 
 #Script Location
@@ -31,4 +31,4 @@ Write-Verbose "Imported CIToolkit"
 
 Merge-XrmSolutionPatches -DisplayName $DisplayName -ParentSolutionUniqueName $ParentSolutionUniqueName -VersionNumber $VersionNumber -ConnectionString $CrmConnectionString -Timeout $Timeout -Verbose
 
-Write-Verbose 'Leaving CloneSolutionAsSolution.ps1'
+Write-Verbose 'Leaving CloneSolution.ps1'
