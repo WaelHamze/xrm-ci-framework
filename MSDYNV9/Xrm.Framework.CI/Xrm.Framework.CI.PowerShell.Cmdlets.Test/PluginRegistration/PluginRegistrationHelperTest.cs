2e386a0c049befb735ecc37f2be52f0ea4995e62
mogvinda <mogvinda@hotmail.com>
2019-01-13 00:04:55 +0000

    Two new commands - Get-XrmPluginRegistrationFromAssembly and Get-XrmPluginRegistrationClass

---
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets.Test/PluginRegistration/PluginRegistrationHelperTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using FakeItEasy;
+using Xrm.Framework.CI.PowerShell.Cmdlets.Common;
+using Xrm.Framework.CI.PowerShell.Cmdlets.PluginRegistration;
+
+namespace Xrm.Framework.CI.PowerShell.Cmdlets.Test
+{
+    [TestFixture]
+    public class PluginRegistrationHelperTest
+    {
+        #region Setup
+        private IReflectionLoader _fakeLoader;
+        private IPluginRegistrationObjectFactory _fakeObjectFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeLoader = A.Fake<IReflectionLoader>();
+            _fakeObjectFactory = A.Fake<IPluginRegistrationObjectFactory>();
+
+            A.CallTo(() => _fakeLoader.Initialise(A<string>._, A<string>._));
+            A.CallTo(() => _fakeObjectFactory.GetAssembly(A<IReflectionLoader>._)).Returns(new Assembly());
+        }
+
+        public void LogVerbose(string text)
+        {
+            //Used in object creation
+        }
+
+        public void LogWarning(string text)
+        {
+            //Used in object creation
+        }
+        #endregion
+
+        [Test]
+        public void GetPluginRegistrationObject_Makes_Expected_Calls()
+        {
+            var pluginRegistrationHelper = new PluginRegistrationHelper(LogVerbose, LogWarning, _fakeLoader, _fakeObjectFactory);
+            pluginRegistrationHelper.GetPluginRegistrationObject("assemblyPath", "customAttributeClass");
+            A.CallTo(() => _fakeLoader.Initialise("assemblyPath", "customAttributeClass")).MustHaveHappened();
+            A.CallTo(() => _fakeObjectFactory.GetAssembly(_fakeLoader)).MustHaveHappened();
+        }
+    }
+}
