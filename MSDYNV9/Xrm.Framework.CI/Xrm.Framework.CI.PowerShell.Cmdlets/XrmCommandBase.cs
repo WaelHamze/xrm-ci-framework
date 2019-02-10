81bd10d65263cf9ca87e511f84eec73060121bda
Mariusz Jakubowski <mariusz.jakubowski@pwc.com>
2019-02-10 19:34:19 +0100

    post-merge fixes

--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/XrmCommandBase.cs
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Cmdlets/XrmCommandBase.cs
@@ -1,16 +1,10 @@
 ﻿using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Tooling.Connector;
-using System;
 using System.Management.Automation;
-using System.Net;
-using System.Threading;
 using Xrm.Framework.CI.Common;
-using Xrm.Framework.CI.Common.Logging;
-using Xrm.Framework.CI.PowerShell.Cmdlets.Logging;
 
 namespace Xrm.Framework.CI.PowerShell.Cmdlets
 {
-    public abstract class XrmCommandBase : PSCmdlet
+    public abstract class XrmCommandBase : CommandBase
     {
         protected virtual IOrganizationService OrganizationService { get; private set; }
 
