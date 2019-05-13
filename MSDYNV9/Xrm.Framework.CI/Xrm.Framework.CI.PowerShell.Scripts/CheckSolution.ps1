1b84beb4a9e908b46730baee57db2bde8dc370d2
Wael Hamze <contact@waelhamze.com>
2019-05-13 10:40:51 +0100

    Update Solution Checker to Latest API

--- MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/CheckSolution.ps1
+++ MSDYNV9/Xrm.Framework.CI/Xrm.Framework.CI.PowerShell.Scripts/CheckSolution.ps1
@@ -11,7 +11,7 @@ param(
 [string]$ApplicationSecret , #The application secret used for connection
 [string]$RulesetId , #The ruleset to be used when checking
 [string]$Geography, #The regional endpoint to hit
-[string]$AzureADPath, #The full path to the Azure AD PowerShell Module
+#[string]$AzureADPath, #The full path to the Azure AD PowerShell Module
 [string]$PowerAppsCheckerPath, #The full path to the PowerApp Checker PowerShell Module
 [bool]$EnableThresholds, #Enables threshold checks
 [string]$ThresholdAction, # Warn, Error - The type of action to generate when number of issues exceeds threshold limit
@@ -34,10 +34,10 @@ Write-Verbose "ApplicationId = $ApplicationId"
 Write-Verbose "ApplicationSecret = $ApplicationSecret"
 Write-Verbose "RulesetId = $RulesetId"
 Write-Verbose "Geography = $Geography"
-Write-Verbose "AzureADPath = $AzureADPath"
+#Write-Verbose "AzureADPath = $AzureADPath"
 Write-Verbose "PowerAppsCheckerPath = $PowerAppsCheckerPath"
 
-Write-Verbose "Importing Azure AD: $AzureADPath"
+#Write-Verbose "Importing Azure AD: $AzureADPath"
 #Import-module "$AzureADPath\AzureAD.psd1"
 
 Write-Verbose "Importing PowerApps Checker: $PowerAppsCheckerPath"
@@ -57,95 +57,68 @@ $CheckParams = @{
 
 $response = Invoke-PowerAppsChecker @CheckParams
 
-$resultFile = Get-ChildItem -Path "$OutputPath" -Filter "*.zip" | Sort CreationTime -Descending | Select -First 1
-
-if ($resultFile)
+if( $response)
 {
-	Add-Type -assembly "System.IO.Compression.FileSystem"
+	$CheckStatus = $response.Status
 
-	try
+	Write-Host "PowerApps Check Status: $CheckStatus"
+	Write-Host "Result File: $($response.DownloadedResultFiles)"
+	Write-Host "Result File Uri: $($response.ResultFileUris)"
+
+	if (($CheckStatus -eq "Finished") -or ($CheckStatus -eq "FinishedWithErrors"))
 	{
-		$resultsZip = [io.compression.zipfile]::OpenRead("$($resultFile.FullName)")
-
-		$jsonFile = $resultsZip.Entries | Where-Object { $_.Name -ilike "*.sarif"}
-
-		if ($jsonFile)
+		if ($CheckStatus -eq "FinishedWithErrors")
 		{
-			try
-			{
-				$reader = New-Object -TypeName System.IO.StreamReader -ArgumentList $jsonFile.Open()
-				$rawContent = $reader.ReadToEnd()
+			Write-Warning "PowerApp Checker finished with errors. Check results for more information"
+		}
 
-				$jsonContent = $rawContent | ConvertFrom-Json
+		Write-Host "============================================" -ForegroundColor DarkGreen
 
-				if ($jsonContent.runs.length -ne 1)
-				{
-					throw "runs count doesn't equal 1"
-				}
-
-				$results = $jsonContent.runs[0].results
-
-				Write-Host "============================================" -ForegroundColor DarkGreen
-				Write-Host "Total number of issues detected: $($results.length)" -ForegroundColor DarkGreen
-				Write-Host "============================================" -ForegroundColor DarkGreen
-
-				$high = @($jsonContent.runs[0].results | Where-Object { $_.properties.engineseverity -eq "High"})
-				$medium = @($jsonContent.runs[0].results | Where-Object { $_.properties.engineseverity -eq "Medium"})
-				$low = @($jsonContent.runs[0].results | Where-Object { $_.properties.engineseverity -eq "Low"})
+		$high = $response.highIssueCount
+		$medium = $response.mediumIssueCount
+		$low = $response.lowIssueCount
 				
-				Write-Host "High:   $($high.length)" -ForegroundColor Green
-				Write-Host "Medium: $($medium.length)" -ForegroundColor Green
-				Write-Host "Low:    $($low.length)" -ForegroundColor Green
-				Write-Host "============================================" -ForegroundColor DarkGreen
+		Write-Host "High:   $high" -ForegroundColor Green
+		Write-Host "Medium: $medium" -ForegroundColor Green
+		Write-Host "Low:    $low" -ForegroundColor Green
+		Write-Host "============================================" -ForegroundColor DarkGreen
 
-				if ($EnableThresholds)
-				{
-					$breached = (
-								($high.Length -gt $HighThreshold) -or
-								($medium.Length -gt $MediumThreshold) -or
-								($low.Length -gt $LowThreshold)
-								)
+		if ($EnableThresholds)
+		{
+			$breached = (
+						($high -gt $HighThreshold) -or
+						($medium -gt $MediumThreshold) -or
+						($low -gt $LowThreshold)
+						)
 					
-					if ($breached)
-					{
-						$msg = "Number of issues detected above threshold limits"
-						if ($ThresholdAction -eq "Warn")
-						{
-							Write-Warning $msg
-						}
-						elseif ($ThresholdAction -eq "Error")
-						{
-							Write-Error $msg
-						}
-					}
-				}
-			}
-			finally
+			if ($breached)
 			{
-				if ($reader)
+				$msg = "Number of issues detected above threshold limits"
+				if ($ThresholdAction -eq "Warn")
 				{
-					Write-Verbose "Disposing of reader"
-					$reader.Dispose()
-					Write-Verbose "Reader Disposed"
+					Write-Warning $msg
+				}
+				elseif ($ThresholdAction -eq "Error")
+				{
+					Write-Error $msg
 				}
 			}
 		}
 	}
-	finally
+	elseif ($CheckStatus -eq "Failed")
 	{
-		if ($resultsZip)
-		{
-			Write-Verbose "Disposing of resultZip"
-			$resultsZip.Dispose()
-			Write-Verbose "resultsZip Disposed"
-		}
+		throw "PowerApps Checker Failed"
+	}
+	else
+	{
+		Write-Warning "Unknown status return from PowerApps Checker"
 	}
 }
 else
 {
-	throw "Couldn't find PowerApps Checker result file"
+	throw "Didn't recieve response from PowerApps Checker"
 }
 
-# End of script
-
 Write-Verbose 'Leaving CheckSolution.ps1'
+
+# End of script
